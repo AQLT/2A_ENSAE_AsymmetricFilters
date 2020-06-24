213af51f252043c581903fabbae5709497a57eab
AQLT <alain.quartier@yahoo.fr>
2020-06-24 22:19:36 +0200

    by kernel ordered factor

--- Filters properties/shinyFunctions.R
+++ Filters properties/shinyFunctions.R
@@ -18,7 +18,62 @@ all_filters <- function(horizon = 3, degree = 3, ic = 4.5){
   names(t) <- c("LC", "QL", "CQ", "CC", "DAF")
   t
 }
+
 coef_plot_comp <- function(filters_properties, endpoints, kernel, q, fixed = "endpoints"){
+  # col_to_plot <- sprintf("q=%i", q)
+  # col_to_plot <- col_to_plot[col_to_plot %in% colnames(filters_properties[[1]][[1]]$filters.coef)]
+  # if (length(col_to_plot) == 0)
+  #   return(NULL)
+  # if(fixed == "endpoints"){
+  #   d <- do.call(rbind,lapply(names(filters_properties[[endpoints]]),function(kernel){
+  #     data <- filters_properties[[endpoints]][[kernel]]$filters.coef
+  #     data <- rbind(data,c(rep(0, ncol(data)-1),NA))
+  #     h <- (nrow(data)-1)/2
+  #     x_values <- sapply(1:ncol(data),function(i){
+  #       c(seq.int(1,h + i-1), rep(h+i, 2), seq_len(h+1-i)+(h+i))
+  #     })
+  #     colnames(x_values) <- colnames(data)
+  #     do.call(rbind,lapply(col_to_plot, function(col){
+  #       data.frame(y = data[,col], 
+  #                  by = kernel, 
+  #                  x = factor(rownames(data)[x_values[,col]],ordered = TRUE,levels = rownames(data)),
+  #                  q = col,
+  #                  stringsAsFactors = F,row.names = NULL)
+  #     }))
+  #   }))
+  # }else{
+  #   d <- do.call(rbind,lapply(names(filters_properties),function(endpoints){
+  #     data <- filters_properties[[endpoints]][[kernel]]$filters.coef
+  #     data <- rbind(data,c(rep(0, ncol(data)-1),NA))
+  #     h <- (nrow(data)-1)/2
+  #     x_values <- sapply(1:ncol(data),function(i){
+  #       c(seq.int(1,h + i-1), rep(h+i, 2), seq_len(h+1-i)+(h+i))
+  #     })
+  #     colnames(x_values) <- colnames(data)
+  #     do.call(rbind,lapply(col_to_plot, function(col){
+  #       data.frame(y = data[,col], 
+  #                  by = endpoints, 
+  #                  x = factor(rownames(data)[x_values[,col]],ordered = TRUE,levels = rownames(data)),
+  #                  q = col,
+  #                  stringsAsFactors = F,row.names = NULL)
+  #     }))
+  #   }))
+  # }
+  # ggplot(data = d, aes(x = x, y = y, group = q, 
+  #                      colour = q)) + 
+  #   geom_line(size = 0.7) + 
+  #   geom_point(size = 1) +
+  #   facet_wrap(~by) +
+  #   theme(panel.background = element_rect(fill = "white", colour = NA), 
+  #         panel.border = element_rect(fill = NA, colour = "grey20"),
+  #         panel.grid.major = element_line(colour = "grey92"), 
+  #         panel.grid.minor = element_line(colour = "grey92", 
+  #                                         size = 0.25),
+  #         strip.background = element_rect(fill = "grey85", colour = "grey20"),
+  #         complete = TRUE, plot.title = element_text(hjust = 0.5),
+  #         legend.title=element_blank()) +
+  #   labs(x = NULL, y = "Coefficients")
+  #####
   if(fixed == "endpoints"){
     d <- do.call(rbind,lapply(names(filters_properties[[endpoints]]),function(kernel){
       data <- filters_properties[[endpoints]][[kernel]]$filters.coef
@@ -31,26 +86,28 @@ coef_plot_comp <- function(filters_properties, endpoints, kernel, q, fixed = "en
       data.frame(data, by = endpoints, x = factor(rownames(data),ordered = TRUE,levels = rownames(data)),
                  stringsAsFactors = F,row.names = NULL)
     }))
+    d$by <- factor(d$by, ordered = TRUE, levels = c("LC", "QL", "CQ", "CC", "DAF"))
   }
-  
+
   colnames(d) <- gsub(".", "=", colnames(d), fixed = TRUE)
   col_to_plot <- sprintf("q=%i", q)
   col_to_plot <- col_to_plot[col_to_plot %in% colnames(d)]
   col_to_plot <- c(col_to_plot, "by", "x")
   if (length(col_to_plot) == 2)
     return(NULL)
-  
+
   d <- d[,col_to_plot]
   dataGraph <- reshape2::melt(d, id = c("x","by"))
-  
-  ggplot(data = dataGraph, aes(x = x, y = value, group = variable, 
-                               colour = variable)) + 
-    geom_line(size = 0.7) + 
+
+  ggplot(data = dataGraph, aes(x = x, y = value, group = variable,
+                               colour = variable)) +
+    geom_line(size = 0.7) +
+    geom_point(size = 1) +
     facet_wrap(~by) +
-    theme(panel.background = element_rect(fill = "white", colour = NA), 
+    theme(panel.background = element_rect(fill = "white", colour = NA),
           panel.border = element_rect(fill = NA, colour = "grey20"),
-          panel.grid.major = element_line(colour = "grey92"), 
-          panel.grid.minor = element_line(colour = "grey92", 
+          panel.grid.major = element_line(colour = "grey92"),
+          panel.grid.minor = element_line(colour = "grey92",
                                           size = 0.25),
           strip.background = element_rect(fill = "grey85", colour = "grey20"),
           complete = TRUE, plot.title = element_text(hjust = 0.5),
