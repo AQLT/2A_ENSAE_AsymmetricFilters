ac5c2d4a6f127a314efc39b5163600d683d32c61
AQLT <alain.quartier@yahoo.fr>
2020-07-22 12:26:24 +0200

    tests filtres asymétriques vs filtres symétriques

--- FST/intuition_sym_asym.R
+++ FST/intuition_sym_asym.R
@@ -1,35 +1,40 @@
 library(rjdfilters)
-small_h <- filterproperties(horizon = 3, kernel = "Henderson", endpoints = "LC", ic = 3.5)
-
-
-compare_filter <- function(x, pdegree = 2, xlim = c(0,pi)){
+compare_filter <- function(x,
+                           hend_horizon = 3, ic = 3.5,
+                           fst_lag = 6, fst_leads = 3, 
+                           pdegree = 2, xlim = c(0,pi)){
+  small_h <- filterproperties(horizon = hend_horizon,
+                              kernel = "Henderson", endpoints = "LC",
+                              ic = ic)
   f <- tryCatch({
-    fstfilter(lags = 6, leads = 3, smoothness.weight = 1/(x+2), timeliness.weight = x/(x+2),
+    fstfilter(lags = fst_lag, leads = fst_leads, smoothness.weight = 1/(x+2), timeliness.weight = x/(x+2),
               pdegree = pdegree)
   },
   error = function(e) {
     print("error")
     NULL
-    })
-
+  })
+  
   if(is.null(f))
     return(NULL)
   # title <- sprintf("timeliness.weight = %.3f, smoothness.weight = fidelity.weight= %.3f",
   #                  x, (1-x)/2)
   title <- sprintf("timeliness.weight = %i, smoothness.weight = fidelity.weight= 1",
                    x)
-  data_matrix <- cbind(diagnostics_matrix(small_h$filters.coef[,"q=3"], lb = 3),
-                       diagnostics_matrix(f$filter, lb = 6))
+  data_matrix <- cbind(diagnostics_matrix(small_h$filters.coef[,sprintf("q=%i",hend_horizon)],
+                                          lb = hend_horizon),
+                       diagnostics_matrix(f$filter, lb = fst_lag))
   data_matrix[nrow(data_matrix),] <- data_matrix[nrow(data_matrix),]*100
   data_matrix <- round(data_matrix,3)
-  colnames(data_matrix) <- c("H3x3","FST6x3")
+  colnames(data_matrix) <- c(sprintf("H%ix%i",hend_horizon, hend_horizon),
+                             sprintf("FST%ix%i",fst_lag, fst_leads))
   layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
-  par(mai = c(0.2, 0.3, 0.2, 0))
+  par(mai = c(0.3, 0.3, 0.2, 0))
   
   plot_coef(f,
             ylim = range(c(-0.15,0.45)),
             main = title, zeroAsNa = TRUE)
-  plot_coef(small_h, q = 3, add = TRUE, col = "red")
+  plot_coef(small_h, q = hend_horizon, add = TRUE, col = "red")
   
   plotrix::addtable2plot(6, 0.5,
                          formatC(data_matrix,
@@ -39,21 +44,26 @@ compare_filter <- function(x, pdegree = 2, xlim = c(0,pi)){
   par(mai = c(0.3, 0.5, 0.2, 0))
   
   plot_gain(f,
-            ylim = range(small_h$filters.gain[,"q=3"]),
-            xlim = xlim)
-  plot_gain(small_h, q = 3, add = TRUE, col = "red")
+            ylim = range(small_h$filters.gain[,sprintf("q=%i",hend_horizon)]),
+            xlim = xlim,
+            main = "gain")
+  plot_gain(small_h, q = hend_horizon, add = TRUE, col = "red")
   abline(v = pi/6, lty = "dotted")
   
   par(mai = c(0.3, 0.5, 0.2, 0))
   
   plot_phase(f,
-             xlim = xlim)
-  plot_phase(small_h, q = 3, add = TRUE, col = "red")
+             xlim = xlim,
+             main = "phase")
+  plot_phase(small_h, q = hend_horizon, add = TRUE, col = "red")
   abline(v = pi/6, lty = "dotted")
 }
+
 pdf(file = "FST/H3x3vsFST6x3.pdf", width=8,height = 5)
 for(x in c(50,seq(100,1000, by = 100),seq(1000,100000, by = 1000))){
-  compare_filter(x, xlim = c(0,pi/6), pdegree = 2)
+  compare_filter(x, xlim = c(0,pi/6), pdegree = 2,
+                 hend_horizon = 3,
+                 fst_lag = 6, fst_leads = 3)
 }
 dev.off()
 
@@ -67,3 +77,14 @@ dev.off()
 compare_filter(1/10,xlim = c(0,pi/6),pdegree = 2)
 compare_filter(0,xlim = c(0,pi/6),pdegree = 2)
 compare_filter(1-1/1000,xlim = c(0,pi/6),pdegree = 2)
+
+
+#########
+small_h <- filterproperties(horizon = 3, kernel = "Henderson", endpoints = "LC", ic = 3.5)
+
+
+pdf(file = "FST/H3x3vsFST6x3.pdf", width=8,height = 5)
+for(x in c(50,seq(100,1000, by = 100),seq(1000,100000, by = 1000))){
+  compare_filter(x, xlim = c(0,pi/6), pdegree = 2)
+}
+dev.off()
