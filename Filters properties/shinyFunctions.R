707d7eebbba33c5809661469b8b4401b1c01d949
AQLT <alain.quartier@yahoo.fr>
2020-07-29 11:17:55 +0200

    update function

--- Filters properties/shinyFunctions.R
+++ Filters properties/shinyFunctions.R
@@ -20,60 +20,6 @@ all_filters <- function(horizon = 3, degree = 3, ic = 4.5){
 }
 
 coef_plot_comp <- function(filters_properties, endpoints, kernel, q, fixed = "endpoints"){
-  # col_to_plot <- sprintf("q=%i", q)
-  # col_to_plot <- col_to_plot[col_to_plot %in% colnames(filters_properties[[1]][[1]]$filters.coef)]
-  # if (length(col_to_plot) == 0)
-  #   return(NULL)
-  # if(fixed == "endpoints"){
-  #   d <- do.call(rbind,lapply(names(filters_properties[[endpoints]]),function(kernel){
-  #     data <- filters_properties[[endpoints]][[kernel]]$filters.coef
-  #     data <- rbind(data,c(rep(0, ncol(data)-1),NA))
-  #     h <- (nrow(data)-1)/2
-  #     x_values <- sapply(1:ncol(data),function(i){
-  #       c(seq.int(1,h + i-1), rep(h+i, 2), seq_len(h+1-i)+(h+i))
-  #     })
-  #     colnames(x_values) <- colnames(data)
-  #     do.call(rbind,lapply(col_to_plot, function(col){
-  #       data.frame(y = data[,col], 
-  #                  by = kernel, 
-  #                  x = factor(rownames(data)[x_values[,col]],ordered = TRUE,levels = rownames(data)),
-  #                  q = col,
-  #                  stringsAsFactors = F,row.names = NULL)
-  #     }))
-  #   }))
-  # }else{
-  #   d <- do.call(rbind,lapply(names(filters_properties),function(endpoints){
-  #     data <- filters_properties[[endpoints]][[kernel]]$filters.coef
-  #     data <- rbind(data,c(rep(0, ncol(data)-1),NA))
-  #     h <- (nrow(data)-1)/2
-  #     x_values <- sapply(1:ncol(data),function(i){
-  #       c(seq.int(1,h + i-1), rep(h+i, 2), seq_len(h+1-i)+(h+i))
-  #     })
-  #     colnames(x_values) <- colnames(data)
-  #     do.call(rbind,lapply(col_to_plot, function(col){
-  #       data.frame(y = data[,col], 
-  #                  by = endpoints, 
-  #                  x = factor(rownames(data)[x_values[,col]],ordered = TRUE,levels = rownames(data)),
-  #                  q = col,
-  #                  stringsAsFactors = F,row.names = NULL)
-  #     }))
-  #   }))
-  # }
-  # ggplot(data = d, aes(x = x, y = y, group = q, 
-  #                      colour = q)) + 
-  #   geom_line(size = 0.7) + 
-  #   geom_point(size = 1) +
-  #   facet_wrap(~by) +
-  #   theme(panel.background = element_rect(fill = "white", colour = NA), 
-  #         panel.border = element_rect(fill = NA, colour = "grey20"),
-  #         panel.grid.major = element_line(colour = "grey92"), 
-  #         panel.grid.minor = element_line(colour = "grey92", 
-  #                                         size = 0.25),
-  #         strip.background = element_rect(fill = "grey85", colour = "grey20"),
-  #         complete = TRUE, plot.title = element_text(hjust = 0.5),
-  #         legend.title=element_blank()) +
-  #   labs(x = NULL, y = "Coefficients")
-  #####
   if(fixed == "endpoints"){
     d <- do.call(rbind,lapply(names(filters_properties[[endpoints]]),function(kernel){
       data <- filters_properties[[endpoints]][[kernel]]$filters.coef
